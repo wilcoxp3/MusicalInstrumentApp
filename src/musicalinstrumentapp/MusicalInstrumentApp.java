ed228c2a37e79c1a5341a456d31a454acc55bdb8
Paul <Paul@192.168.2.5>
2015-02-26 00:16:03 -0500

    new commit

---
+++ src/musicalinstrumentapp/MusicalInstrumentApp.java
@@ -0,0 +1,16 @@
+/*
+ * Workbook Project Handout 2
+ * Professor: Tedd Sperling
+ * Course: Introduction to JAVA - (22150)
+ * Student: Paul Wilcox
+*/
+package musicalinstrumentapp;
+
+public class MusicalInstrumentApp
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
