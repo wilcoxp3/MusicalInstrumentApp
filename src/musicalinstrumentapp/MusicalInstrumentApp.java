9fac677cc4d086555173de3b3d214928df01335e
Paul <Paul@192.168.2.5>
2015-02-26 00:52:08 -0500

    new commit

--- src/musicalinstrumentapp/MusicalInstrumentApp.java
+++ src/musicalinstrumentapp/MusicalInstrumentApp.java
@@ -10,7 +10,6 @@ public class MusicalInstrumentApp
 {
     public static void main(String[] args)
     {
-        //here are some changes right here
-        //and here are some more
+        
     }
 }
